6b2115dcc360502e771a2c42e95d094c7355bf1b
musaeri1807 <musaeri1807@gmail.com>
2023-07-31 21:35:55 +0700

    first commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // default
-$route['default_controller'] = 'Auth';
+$route['default_controller'] = 'auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
